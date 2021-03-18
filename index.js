4a923fb97dee5c7cbf9fde1700050b242ee6dd46
tino73 <altin.thaqi.520@gmail.com>
2021-03-18 19:14:14 +0100

    Added Configs needed to deplot to heroku

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 var Anime = require("anime-scraper").Anime;
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 //Static files
